ac61e421f4803b6086c5e786233ef9b60d8fdbbf
Kayla Ondracek <kayla.renee@gmail.com>
2018-05-08 15:28:44 -0700

    add jobject objects for message display

---
+++ Twilio/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Twilio
+{
+    public class Message
+    {
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Body { get; set; }
+        public string Status { get; set; }
+    }
+}
