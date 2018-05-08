1fadb574b4dab93f9d6ca3a5f544a571fe5a3ed7
Kayla Ondracek <kayla.renee@gmail.com>
2018-05-08 14:28:39 -0700

    add console app that gets messages

--- Twilio/Program.cs
+++ Twilio/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Twilio
 {
@@ -6,7 +11,33 @@ namespace Twilio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new RestClient("https://api.twilio.com/2010-04-01");
+            
+
+            var request = new RestRequest("Accounts/ACa4f8544a68ff139fcdb465bbb8d6a814/Messages.json", Method.GET);
+            client.Authenticator = new HttpBasicAuthenticator("ACa4f8544a68ff139fcdb465bbb8d6a814", "b33429a73f6f1171c7a9c2ee3949ef4a");
+
+            var response = new RestResponse();
+
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            Console.WriteLine(jsonResponse["messages"]);
+            Console.ReadLine();
         }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+
     }
 }
